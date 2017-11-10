fc3423470778010c6f711ba74caf6aa5b7a89684
Claudio Pereira <claudiocleberson@hotmail.com>
2017-11-10 17:48:45 -0200

    commit 2

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import LandPage from '@/components/Landpage/LandPage.vue'
-import Home from '@/components/Home.vue'
-import CreateMeetup from '@/components/Meetup/CreateMeetup.vue'
-import Meetups from '@/components/Meetup/Meetups.vue'
-import Profile from '@/components/User/Profile.vue'
-import SignUp from '@/components/User/SignUp.vue'
-import SignIn from '@/components/User/SignIn.vue'
-import Meetup from '@/components/Meetup/Meetup.vue'
-import Privacy from '@/components/TermPrivacy/privacy.vue'
-import Terms from '@/components/TermPrivacy/term.vue'
-import AuthGuard from './auth-guard'
-Vue.use(Router)
+import Vue from 'vue';
+import Router from 'vue-router';
+import LandPage from '../components/Landpage/LandPage.vue';
+import Home from '../components/Home.vue';
+import CreateMeetup from '../components/Meetup/CreateMeetup.vue';
+import Meetups from '../components/Meetup/Meetups.vue';
+import Profile from '../components/User/Profile.vue';
+import SignUp from '../components/User/SignUp.vue';
+import SignIn from '../components/User/SignIn.vue';
+import Meetup from '../components/Meetup/Meetup.vue';
+import Privacy from '../components/TermPrivacy/privacy.vue';
+import Terms from '../components/TermPrivacy/term.vue';
+import AuthGuard from './auth-guard';
+Vue.use(Router);
 
 export default new Router({
   routes: [
@@ -70,4 +70,4 @@ export default new Router({
     }
   ],
   mode: 'history'
-})
+});
