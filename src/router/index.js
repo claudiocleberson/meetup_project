5e1c41d008a05b344c67af445423f9c8c48d413d
Claudio Pereira <claudiocleberson@hotmail.com>
2017-11-11 07:57:50 -0200

    changed routers

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import LandPage from '../components/Landpage/LandPage.vue';
 import Home from '../components/Home.vue';
 import CreateMeetup from '../components/Meetup/CreateMeetup.vue';
 import Meetups from '../components/Meetup/Meetups.vue';
@@ -10,16 +9,12 @@ import SignIn from '../components/User/SignIn.vue';
 import Meetup from '../components/Meetup/Meetup.vue';
 import Privacy from '../components/TermPrivacy/privacy.vue';
 import Terms from '../components/TermPrivacy/term.vue';
+import LandPage from '../components/Landpage/LandPage.vue';
 import AuthGuard from './auth-guard';
 Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/landpage',
-      name: 'Landpage',
-      component: LandPage
-    },
     {
       path: '/',
       name: 'Home',
